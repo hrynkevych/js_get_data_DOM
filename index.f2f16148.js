b7c4c33bdca90b97f5b932482b0c6b67ca381b0d
grinjk1993 <grinjk1993@gmail.com>
2023-12-28 22:41:02 +0100

    make build

---
+++ index.f2f16148.js
@@ -0,0 +1,3 @@
+var t=document.querySelectorAll(".population"),e=document.querySelector(".total-population"),o=document.querySelector(".average-population"),n=0;t.forEach(function(t){var e=t.textContent.split(",").join("");n+=+e});var l=n/t.length;e.textContent=n.toLocaleString("en-US"),o.textContent=l.toLocaleString("en-US");//# sourceMappingURL=index.f2f16148.js.map
+
+//# sourceMappingURL=index.f2f16148.js.map
